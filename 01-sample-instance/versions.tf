01dfeabf65bf5426ec576084cac8966db439acc1
ChaitanyaReddy05 <49510138+ChaitanyaReddy05@users.noreply.github.com>
2021-12-27 16:29:31 +0530

    Update versions.tf

--- 01-sample-instance/versions.tf
+++ 01-sample-instance/versions.tf
@@ -5,20 +5,6 @@ terraform {
       version = "=2.46.0"
     }
   }
-    backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "tfstateoce"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-
+  
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "eastus"
-}
